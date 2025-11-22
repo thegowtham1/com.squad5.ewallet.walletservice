09b46e7183176901642591ce2f8ae6435eaa3628
gowtham <thegowtham.contact@gmail.com>
2025-11-22 16:29:07 +0530

    initial commit
    Wallet Service initial commit

--- src/main/java/com/squad5/ewallet/walletservice/EWalletService.java
+++ src/main/java/com/squad5/ewallet/walletservice/EWalletService.java
@@ -1,13 +1,13 @@
-package com.thegowtham.springapp;
+package com.squad5.ewallet.walletservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringappApplication {
+public class EWalletService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringappApplication.class, args);
+		SpringApplication.run(EWalletService.class, args);
 		System.out.println("Started");
 	}
 
--- src/test/java/com/squad5/ewallet/walletservice/EWalletServiceTests.java
+++ src/test/java/com/squad5/ewallet/walletservice/EWalletServiceTests.java
@@ -1,10 +1,10 @@
-package com.thegowtham.springapp;
+package com.squad5.ewallet.walletservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringappApplicationTests {
+class EWalletServiceTests {
 
 	@Test
 	void contextLoads() {
